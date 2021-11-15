26202a9fcd38194c30ce834b60617e067e16e722
YOUSSEF ACHEHBOUNE <94069079+youssefachehboune@users.noreply.github.com>
2021-11-15 23:10:36 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,57 +1,32 @@
-#!bin/bash
-
-## Command substitutions
-ARCH=$(uname -a)
-PCPU=$(grep 'physical id' /proc/cpuinfo | wc -l)
-VCPU=$(grep 'processor' /proc/cpuinfo | wc -l)
-FULLRAM=$(free -m | grep Mem: | awk '{print $2}')
-USEDRAM=$(free -m | grep Mem: | awk '{print $3}')
-PCTRAM=$(free -m | grep Mem: | awk '{printf("%.2f"), $3/$2*100}')
-FULLDISK=$(df -Bg | grep /dev/ | grep -v /boot | awk '{fd += $2} END {print fd}')
-USEDDISK=$(df -Bm | grep /dev/ | grep -v /boot | awk '{ud += $3} END {print ud}')
-PCTDISK=$(df -Bm | grep /dev/ | grep -v /boot | awk '{ud += $3} {fd += $2} END {printf("%d"), ud/fd*100}')
-CPU=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-LASTBOOT=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-LVM=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
-TCP=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
-TCPMSSG=$(if [ ${TCP} -eq 0 ]; then echo NOT ESTABLISHED; else echo ESTABLISHED; fi)
-USERLOG=$(users | wc -w)
-IP=$(hostname -I)
-MAC=$(ip link show | awk '$1 == "link/ether" {print $2}')
-SUDO=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-
-## Shows the architecture of the operating system and its kernel version
-echo "#Architecture: ${ARCH}"
-
-## Shows the number of physical processors (CPUs)
-echo "#CPU physical: ${PCPU}"
-
-## Shows the number of virtual processors (vCPUs)
-echo "#vCPU: ${VCPU}"
-
-## Shows the current available RAM on your server and its utilization rate as percentage
-echo "#Memory Usage: ${USEDRAM}/${FULLRAM}MB (${PCTRAM}%)"
-
-## Shows the current available memory on your server and its utilization rate as a percentage
-echo "#Disk Usage: ${USEDDISK}/${FULLDISK}Gb (${PCTDISK}%)"
-
-## Shows the current utilization rate of your processors as a percentage
-echo "#CPU load: ${CPU}"
-
-## Shows the date and time of the last reboot
-echo "#Last boot: ${LASTBOOT}"
-
-## Shows whether LVM is active or not
-echo "#LVM use: ${LVM}"
-
-## Shows the number of active connections
-echo "#Connections TCP: ${TCP} ${TCPMSSG}"
-
-## Shows the number of users using the server
-echo "#User log: ${USERLOG}"
-
-## Shows the IPv4 address of your server and its MAC (Media Access Control) address
-echo "#Network: IP ${IP} (${MAC})"
-
-## Shows the number of commands executed with the sudo program
-echo "#Sudo: ${SUDO} cmd"
+#!/bin/bash
+arc=$(uname -a)
+pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
+udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
+pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
+cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lvmt=$(lsblk | grep "lvm" | wc -l)
+lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
+#You need to install net tools for the next step [$ sudo apt install net-tools]
+ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
+ulog=$(users | wc -w)
+ip=$(hostname -I)
+mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
+cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l) # journalctl should be running as sudo but our script is running as root so we don't need in sudo here
+wall "	#Architecture: $arc
+	#CPU physical: $pcpu
+	#vCPU: $vcpu
+	#Memory Usage: $uram/${fram}MB ($pram%)
+	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
+	#CPU load: $cpul
+	#Last boot: $lb
+	#LVM use: $lvmu
+	#Connexions TCP: $ctcp ESTABLISHED
+	#User log: $ulog
+	#Network: IP $ip ($mac)
+	#Sudo: $cmds cmd" # broadcast our system information on all terminals
